613225a8810daa79524b685aa3c00440c1ee4860
haroon abdu saeed <aliafhfdrtg@gmail.com>
2024-10-23 18:48:20 +0300

    first commit

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,18 @@
+<!-- footer starts -->
+<footer class="footer">
+<div class="d-sm-flex justify-content-center justify-content-sm-between text-center">
+    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © HAROON ALRASHEED  
+        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">
+<a href="https://wa.me/00966570625285" target="_blank" class="text-muted"> 00966570625285</a> 2024
+| <a href="mailto:haroonabdus@gmail.com" class="text-muted">haroonabdus@gmail.com</a>
+
+        </span>
+    </span>
+    {{-- <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">
+        Copyright © <a href="https://wa.me/رقم_الواتس" target="_blank" class="text-muted">هارون الرشيد</a> 2024
+    </span> --}}
+   
+</div>
+</div>
+</footer>
+<!-- footer ends -->
